17ec30a3f94c4c0f4793879b2e2509fd38adca13
Aleksandar Ignjatijevic <a.ignjatijevic@gmail.com>
2021-04-15 00:07:18 +0200

    :lipstick: Frontend for login, registration and home page

--- frontend/webshop/src/app/app-routing.module.ts
+++ frontend/webshop/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
 },
 {
@@ -16,6 +17,10 @@ const routes: Routes = [
 {
   path: 'login',
   component: LoginComponent
+},
+{
+  path:'registration',
+  component: RegisterComponent
 }
 ];
 
