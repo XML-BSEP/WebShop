90ba9c19d2356c8e459f05b96d1fc5a7387596d1
Bojan Vujic <58082605+VujicGit@users.noreply.github.com>
2021-04-16 00:02:49 +0200

    :sparkles: Implemented redis service

--- src/infrastructure/redisdb/configuration.go
+++ src/infrastructure/redisdb/configuration.go
@@ -6,7 +6,7 @@ import (
 )
 
 func init_viper() {
-	viper.SetConfigFile(`src/configurations/dbconfig.json`)
+	viper.SetConfigFile(`src/configurations/redis.json`)
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
@@ -17,12 +17,11 @@ func init_viper() {
 
 func NewReddisConn() *redis.Client {
 	init_viper()
-	//address := viper.GetString(`server.address`)
-	//port := viper.GetString(`server.port`)
+	address := viper.GetString(`server.address`)
+	port := viper.GetString(`server.port`)
 
-	//redisAddr := address + ":" + port
 	return redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr: address + ":" + port,
 		Password: "",
 		DB: 0,
 	})
