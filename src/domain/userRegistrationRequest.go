03deb389a1de811e6ca0ec37aee5918ab74953af
Bojan Vujic <58082605+VujicGit@users.noreply.github.com>
2021-04-17 00:03:18 +0200

    :construction: Added custom validators and error messages

--- src/domain/userRegistrationRequest.go
+++ src/domain/userRegistrationRequest.go
@@ -2,13 +2,13 @@ package domain
 
 //TODO stavi u malo
 type UserRegistrationRequest struct {
-	Email	string	`json:"email"`
-	SecurityQuestion	string	`json:"question"`
-	SecurityAnswer	string	`json:"answer"`
-	Name	string	`json:"name"`
-	Surname	string	`json:"surname"`
-	Username     string	`json:"username"`
-	Password   string	`json:"password"`
-	VerificationCode	string `json:"code"`
+	Email	string	`json:"email" validate:"required,email"`
+	SecurityQuestion	string	`json:"question" validate:"required"`
+	SecurityAnswer	string	`json:"answer" validate:"required"`
+	Name	string	`json:"name" validate:"required"`
+	Surname	string	`json:"surname" validate:"required"`
+	Username     string	`json:"username" validate:"required,min=4"`
+	Password   string	`json:"password" validate:"required"`
+	VerificationCode	string `json:"code" validate:"required"`
 
 }
