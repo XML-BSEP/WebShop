c53c9cc8f9998a8e2ed25a9513c7561f8f76f61c
st3fan7 <58364787+st3fan7@users.noreply.github.com>
2021-04-20 01:42:17 +0200

    :construction: :sparkles: Added mail confirmation on frontend

--- frontend/webshop/src/app/registration-confirmation/registration-confirmation.component.ts
+++ frontend/webshop/src/app/registration-confirmation/registration-confirmation.component.ts
@@ -1,5 +1,8 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { RegistrationService } from '../service/registration/registration.service';
+import {ConfirmRegistration} from '../model/confirmRegistration'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration-confirmation',
@@ -8,16 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationConfirmationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router, private registrationService : RegistrationService) { }
   public codeForm : FormGroup;
+  private userMail : string;
+  private confirmRegistration : ConfirmRegistration
+
+
   ngOnInit(): void {
     this.codeForm = new FormGroup({
       //maybe add pattern for code validation on frontend
       'code' : new FormControl(null, [Validators.required])
     });
 
+    this.userMail = history.state.data;
+
   }
   confirm(){
+    console.log(this.userMail)
+    this.confirmRegistration = new ConfirmRegistration(this.userMail, this.codeForm.controls.code.value)
+
+    this.registrationService.confAcc(this.confirmRegistration).subscribe(
+      res=>{
+        alert("Successful confirmation")
+        this.router.navigate(['/login']);
+      }
+      
+      
+        )
 
   }
   resend(){
--- frontend/webshop/src/app/service/registration/registration.service.ts
+++ frontend/webshop/src/app/service/registration/registration.service.ts
@@ -2,6 +2,7 @@ import { environment } from './../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisteredUser } from 'src/app/model/registeredUser';
+import { ConfirmRegistration} from 'src/app/model/confirmRegistration'
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,12 @@ import { RegisteredUser } from 'src/app/model/registeredUser';
 export class RegistrationService {
 
   constructor(private https : HttpClient) { }
+
   registerPatient(data : RegisteredUser){
     return this.https.post(`${environment.baseUrl}/${environment.registration}`,data, {responseType : 'text'});
+  }
+
+  confAcc(data : ConfirmRegistration){
+    return this.https.post(`${environment.baseUrl}/${environment.confirmAccount}`,data, {responseType : 'text'});
   }
 }
